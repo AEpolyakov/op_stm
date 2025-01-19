fdff1650031ac7614af8ae9e8f8df237bda696a1
Alexey Polyakov <aypolyakov@mts.ru>
2025-01-19 17:09:32 +0400

    utils

---
+++ Core/Inc/utils.h
@@ -0,0 +1,15 @@
+/*
+ * utils.h
+ *
+ *  Created on: Jan 8, 2025
+ *      Author: Alexey
+ */
+
+#ifndef INC_UTILS_H_
+#define INC_UTILS_H_
+
+
+void delay(int del);
+
+
+#endif /* INC_UTILS_H_ */
---
+++ Core/Src/utils.c
@@ -0,0 +1,11 @@
+/*
+ * utils.c
+ *
+ *  Created on: Jan 8, 2025
+ *      Author: Alexey
+ */
+
+void delay(int del){
+	static int i;
+	for (i=0; i<del; i++) {}
+}
